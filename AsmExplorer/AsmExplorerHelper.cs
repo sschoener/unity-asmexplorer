75f211ca25fa17249d6e21da7b09f50b4a3c6dc1
Sebastian Schöner <sebastian.schoner@unity3d.com>
2020-01-09 18:23:36 +0100

    Fix disposed object exception

--- AsmExplorer/AsmExplorerHelper.cs
+++ AsmExplorer/AsmExplorerHelper.cs
@@ -2,40 +2,46 @@
 using UnityEngine;
 
 namespace AsmExplorer {
-	[ExecuteAlways]
-	class AsmExplorerHelper : MonoBehaviour {
-		static WebService s_WebService;
+    [ExecuteAlways]
+    class AsmExplorerHelper : MonoBehaviour {
+        static WebService s_WebService;
 
-		void Awake() {
-			RestartWebservice();
-		}
+        static void RestartWebservice()
+        {
+            s_WebService?.Stop();
+            s_WebService = new WebService(new Explorer(), "explorer");
+            s_WebService.Start();
+            UnityEngine.Debug.Log("(Re)Starting web service");
+        }
+        
+        static void StopWebservice()
+        {
+            s_WebService?.Stop();
+            s_WebService = null;
+        }
 
-		static void RestartWebservice()
-		{
-			s_WebService?.Stop();
-			s_WebService = new WebService(new Explorer(), "explorer");
-			s_WebService.Start();
-			UnityEngine.Debug.Log("(Re)Starting web service");
-		}
+        void Awake() {
+            RestartWebservice();
+        }
 
-		void OnEnable()
-		{
-			RestartWebservice();
-		}
+        void OnEnable()
+        {
+            RestartWebservice();
+        }
 
-		void OnDisable()
-		{
-			s_WebService?.Stop();
-		}
+        void OnDisable()
+        {
+            StopWebservice();
+        }
 
-		void OnDestroy()
-		{
-			s_WebService?.Stop();
-		}
+        void OnDestroy()
+        {
+            StopWebservice();
+        }
 
-		void OnApplicationQuit() {
-			s_WebService?.Stop();
-		}
-	}
+        void OnApplicationQuit() {
+            StopWebservice();
+        }
+    }
 
 }
